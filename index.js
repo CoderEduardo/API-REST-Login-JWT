1b4c32d85675fbebc9bfe5a4fa3d9bf3048cb5c3
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-20 17:22:43 -0300

    Conectando ao banco de dados

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express")
 const app = express()
 const PORTA = 8080
 const userRouter = require("./routes/userRouter")
+const mongoose = require("mongoose")
+const URL = "mongodb://127.0.0.1/teste"
+
+mongoose.connect(URL).then(()=>{
+    console.log("Banco de dados conectado com sucesso")
+}).catch(erro=>{
+    console.log(`Falha ao se conectar: ${erro}`)
+})
 
 app.use("/",userRouter)
 
