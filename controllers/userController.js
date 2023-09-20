e410c92c7986d8a0c0edc88be5c5e1e4b2058665
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-20 18:07:53 -0300

    Cryptografando a senha e impedindo a criação de emails duplicados

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,21 @@
 const User = require("../models/User")
+const bcrypt = require("bcryptjs")
 
 const userController = {
     registrar: async (req,res)=>{
+
+        let verificarEmail = await User.findOne({email:req.body.email})
+        if(verificarEmail) return res.status(400).send("Esse email já está cadastrado")
+
         const user = new User({
             name:req.body.name,
             email:req.body.email,
-            password:req.body.password
+            password:bcrypt.hashSync(req.body.password)
         })
 
         try{
-            await user.save()
+            let saveUser = await user.save()
+            res.send(saveUser)
         }catch(erro){
             res.send(erro).res.status(400)
         }
